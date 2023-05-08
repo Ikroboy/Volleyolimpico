ee77ac666b8ca3634bbc5481d53d931b301a787a
Icaro Coutinho <83377159+Ikroboy@users.noreply.github.com>
2023-05-08 17:14:34 -0300

    Update background.cycle.js

--- js/background.cycle.js
+++ js/background.cycle.js
@@ -2,20 +2,12 @@ var currentImageIndex = -1;
 var imageIds = new Array();
 var fadeSpeed;
 
-//Sizing constants. these determine the value of the CSS property 'background-size' of the selected container
-var SCALING_MODE_NONE = 0; //Uses the original image size
-var SCALING_MODE_STRETCH = 1; //Sets 'background-size' to '100% 100%'. This stretches the background image to fill the container, discarding the images aspect ratio.
-var SCALING_MODE_COVER = 2; //Sets 'background-size' to 'cover'. This makes the background images fill the entire container while retaining its aspect ratio.
-var SCALING_MODE_CONTAIN = 3; //Sets 'background-size' to 'contain'. This scales the bakcground image to the largest size such that both its width and its height can fit inside the content area
 
-/**
- * Adds a cycling (fading) background to the selected element
- * @param {Object} options Options for tweaking the cycle setings.
- * imageUrls: an array of strings representing urls to the images to cycle through
- * duration: the nr of miliseconds between two fades.
- * fadeSpeed: the nr of miliseconds it takes for one image to fade out to another.
- * backgroundSize: specify a value for the css3 property 'background size' or one of the following constants; SCALING_MODE_NONE, SCALING_MODE_STRETCH, SCALING_MODE_COVER, SCALING_MODE_CONTAIN
- */
+var SCALING_MODE_NONE = 0; 
+var SCALING_MODE_STRETCH = 1; 
+var SCALING_MODE_COVER = 2; 
+var SCALING_MODE_CONTAIN = 3; 
+
 $.fn.backgroundCycle = function(options) {
   var settings = $.extend(
     {
